cf9c6ed0bd0d8342c3aa0fb235b946aae59f0dfd
LinShan-Bin <zhangchubin1@gmail.com>
2025-10-26 20:43:25 +0800

    init: add gaussian modules

---
+++ gaussian_modules/backbone_2d/__init__.py
@@ -0,0 +1,13 @@
+from .backbone2d import (
+    GaussianImageBackbone, 
+    GaussianImageFeatureExtractor,
+    GaussianDetectionHead,
+    OptimizedLSSBasedTPVGeneratorV2
+)
+
+__all__ = [
+    'GaussianImageBackbone', 
+    'GaussianImageFeatureExtractor',
+    'GaussianDetectionHead',
+    'OptimizedLSSBasedTPVGeneratorV2'
+]
